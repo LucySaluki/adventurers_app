5e1a1e53c0c0f9fd1bed45ac0dada7e27b538146
LucySaluki mslucyarnold@gmail.com <mslucyarnold@gmail.com>
2020-08-08 13:14:37 +0100

    database set up

---
+++ db/adventures.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS places;
+DROP TABLE IF EXISTS countries;
+
+CREATE TABLE countries (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+CREATE TABLE places (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR (255),
+    description VARCHAR(255),
+    place_type VARCHAR(255),
+    country_id INT REFERENCES countries(id)
+)
