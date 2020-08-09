b1b6e5a0c1627f43021440acea0904a87857f4ca
LucySaluki mslucyarnold@gmail.com <mslucyarnold@gmail.com>
2020-08-09 15:17:19 +0100

    add new country

--- controllers/countries_controller.py
+++ controllers/countries_controller.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, Flask, render_template
+from flask import Blueprint, Flask, render_template, request, redirect
 
 from models.countries import Country
 import repositories.country_repository as country_repository
@@ -10,3 +10,17 @@ countries_blueprint = Blueprint("countries", __name__)
 def countries():
     countries = country_repository.select_all()
     return render_template("countries/index.html", countries=countries)
+
+# NEW
+@countries_blueprint.route("/countries/new")
+def new_country():
+    return render_template("countries/new.html")
+
+
+# CREATE
+@countries_blueprint.route("/countries", methods=["POST"])
+def create_country():
+    name = request.form["name"]
+    new_country = Country(name)
+    country_repository.save(new_country)
+    return redirect("/countries")
